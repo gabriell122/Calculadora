60c18866226bec32b9cbb626f1e3578fb000b839
gabriellHaines <gabriell.haines@etec.sp.gov.br>
2024-06-24 17:27:48 -0300

    up

--- index.js
+++ index.js
@@ -1,3 +1,183 @@
+//sinalCorrente VAI GERAR UM BUG NO ()
+
+
+//Define o visor
+const visor = document.getElementById('visor')
+
+//Define uma variável para armanezar o Número Corrente
+let numeroCorrente = "";
+
+//Cria um Array para gardar os Números e Sinais da Conta já separados
+let contaArray = [];
+
+//Cria uma variável para gardar o Sinal Corrente permitindo a troca de Sinais
+let sinalCorrente = "";
+
+//Defino os Números e Sinais de operações
+const nummero = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
+const sinal = ["-", "+", "*", "/", "^", "$", "m", "n", "f", "%"];
+
+const BotaoPrecionado = (botao) => {
+
+    //Se for um Número
+    if (nummero.includes(botao)) {
+
+        //Executa 
+        Numero(botao);
+
+        //Se for um Sinal
+    } else if (sinal.includes(botao)) {
+
+        //Executa
+        Sinal(botao);
+
+        //Se for =
+    } else if (botao === "=") {
+
+        Igual();
+        //Entrada não tratada
+    } else {
+        console.log('Erro');
+        console.log('Entrada não tratada');
+    }
+
+}
+
+//Se for um Número
+const Numero = (botao) => {
+    //Adiciona o Número prescionado ao Número Corrente
+    numeroCorrente = numeroCorrente + botao;
+
+    //Exibe o calculo na tela
+    visor.innerHTML = contaArray.join(' ') + ' ' + sinalCorrente + ' ' + numeroCorrente;
+
+    //Se tiver um sinal gardado Adiciona ele no Array da conta e zera a variável do sinal
+    if (sinalCorrente) {
+        contaArray.push(sinalCorrente);
+        sinalCorrente = ""
+    }
+}
+
+//Se for um Sinal
+const Sinal = (botao) => {
+
+    //Acrecenta ao array o Número  se ele existir e zera a variável do Número
+    if (numeroCorrente) {
+        contaArray.push(parseFloat(numeroCorrente));
+        numeroCorrente = '';
+    }
+
+    //Sinal Corrente resebe o Botão pressionado
+    sinalCorrente = botao;
+
+    //verifico sinais  que não da para pasar valor direto        
+    switch (botao) {
+
+        //Troca M por ( 
+        case 'm':
+            botao = '(';
+            break;
+
+        //Troca N por ) 
+        case 'n':
+            botao = ')';
+            break;
+
+        // Apaga a memória resetando o contaArray, sinalCorrente e o numeroCorrente
+        case 'f':
+            contaArray = [];
+            numeroCorrente = '';
+            sinalCorrente = "";
+            break;
+
+        //Sai do Switch
+        default:
+            break;
+    }
+
+    //Exibe o Array mais o Sinal pressionado
+    visor.innerHTML = contaArray.join(' ') + " " + sinalCorrente;
+}
+
+const Igual = ()=>{
+
+    //Verifica se não tem um Número perdido
+    if (numeroCorrente) {
+
+        //Adiciona ele no Array
+        contaArray.push(parseFloat(numeroCorrente));
+        
+        //reseta o numeroCorrente
+        numeroCorrente = '';
+    }
+
+    //Verifica se não tem um Sinal perdido
+    if(sinalCorrente){
+
+        //Adiciona ele no Array
+        contaArray.push(sinalCorrente);
+        
+        //reseta o sinalCorrente
+        sinalCorrente = "";
+    }
+
+    const Resposta = Calculo();
+
+}
+
+//Faz o calculo
+const Calculo = ()=>{
+    contaArray.map(
+        
+    )
+
+}
+
+
+//Função que verifica os parênteses 
+const Ordem2 = (arrayMap) => {
+
+    //Roda o arrayMap buscando por parênteses (pela ultima abertura)
+    for (let index = arrayMap.length + 1; index >= 0; index--) {
+
+        //se achar
+        if (arrayMap[index] == '(') {
+            let aIndex = index;
+
+            //busca pelo primeiro fechamento
+            for (let index = aIndex; index < arrayMap.length; index++) {
+
+                //se achar o fechamento
+                if (arrayMap[index] == ')') {
+
+                    //corta o arrayMap gerando um novo arraySlice que é o recorte interno do conteudo do parênteses
+                    let arraySlice = arrayMap.slice(aIndex + 1, index);
+
+                    //calcula o valor do arraySlice
+                    const Res = Calcular(arraySlice);
+                    //ranca a parte do parênteses que foi calculado 
+                    arrayMap.splice(aIndex, index - aIndex + 1);
+                    //adiciona o valor Resultado
+                    arrayMap.splice(aIndex, 0, Res[0]);
+                } else {
+                    console.log('Erro: fechamento de parênteses');
+                }
+            }
+        }
+    }
+
+    // depois que todos os parenteses são calculado então envia o array principal para ser calculado
+    const Resultado = Calcular(arrayMap);
+    //pega a resposta retorna ela
+    return (Resultado[0]);
+    //atuliza o valor de cal para o resposta
+    cal = array[0]
+}
+
+
+
+
+
 //Var global
 let currentNumber = '';
 let arrayCount = [];
@@ -6,9 +186,7 @@ const htmlHitorico = document.getElementById("history");
 let sinalPress = "";
 const PresButton = (presButton) => {
 
-    //defino os numero e sinais de operações
-    const num = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "."];
-    const sinal = ["-", "+", "*", "/", "^", "$", "m", "n", "f", "%"];
+
 
     //se for numero
     if (num.includes(presButton)) {
